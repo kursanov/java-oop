2cdb17a814fb886d83afac50bd35d7852cc27925
kursanov <z.kursanov@yandex.ru>
2022-07-06 12:14:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Uchburch uch = new Uchburch();
+        uch.birinchi = 34;
+        uch.ekinchi = 12;
+        uch.uchuncu = 20;
+        System.out.print("Ayant: ");
+        uch.arae();
+    }
+}
---
+++ src/com/company/Uchburch.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Uchburch {
+
+    int birinchi;
+    int ekinchi;
+    int uchuncu;
+
+    void arae (){
+        int s = birinchi + ekinchi + uchuncu;
+        System.out.println(s);
+    }
+}
